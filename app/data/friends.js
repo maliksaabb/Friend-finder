0eb66ee001d6bf9c7d2ad9449bafb640bcc95a55
usmanmalik <usmandotmalik@gmail.com>
2019-11-05 20:34:20 -0500

    updating

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendList = [
+  {
+    name: 'Molly Levine',
+    profilePic: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/36433950_10156349011402969_8655522797059047424_o.jpg?_nc_cat=109&_nc_ht=scontent-iad3-1.xx&oh=303b957d1557552d23daa50c831d3ede&oe=5D6D5BC1',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
+  
